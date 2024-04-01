91b8182e9ebdad92ca4e3c5fc5d275be01263cb7
swayam_op <swayamprakash97691@gmail.com>
2024-04-01 20:08:56 +0530

    v5

--- api/index.js
+++ api/index.js
@@ -1,104 +1,24 @@
 import { configDotenv } from "dotenv";
-// import { connectDb } from "../src/DB/index.js";
-// import { server } from "../src/app.js";
-// import { setUpCloudinary } from "../src/utils/Cloudnary.js";
-
-import cors from "cors";
-import express from "express";
-import { Server } from 'socket.io';
-import {createServer} from 'http';
+import { connectDb } from "../src/DB/index.js";
+import { server } from "../src/app.js";
+import { setUpCloudinary } from "../src/utils/Cloudnary.js";
 
 configDotenv({path:'.env'});
-const app = express();
 
-const corsOptions ={
-    origin: process.env.FRONT_END_URL ||  "http://localhost:3000", 
-    methods: ["GET", "POST", "PUT", "DELETE"],
-    credentials:true,            //access-control-allow-credentials:true
-    optionSuccessStatus:200
-};
-app.use(cors(corsOptions));
-app.use(express.json());
-app.use(express.static('/public'));
 
-app.get('/',(req, res)=>{
-    res.json("hellow")
+//Connect Database
+
+connectDb()
+.then(()=>{
+    console.log("Connected to mongodb");
+    setUpCloudinary();
+    server.listen(process.env.PORT,()=>{
+        console.log(`server is listening on ${process.env.PORT}`);
+    });
 })
-
-const server = createServer(app);
-
-server.listen(process.env.PORT,()=>{
-    console.log("server is listening", );
+.catch((error)=>{
+    console.log("mongodb error",error)
 })
-
-// const io = new Server(server, {
-//     cors: {
-//       origin: process.env.FRONT_END_URL ||  "http://localhost:3000",
-//       methods: ["GET", "POST", "PUT", "DELETE"],
-//       credentials: true,
-//     },
-//   });
-
-// //import routers
-// import userRouter from '../src/routers/User.Router.js';
-// import AuthRouter from "../src/routers/Auth.Router.js";
-// import CodeRouter from "../src/routers/Code.Router.js";
-// import QuestionRouter from "../src/routers/Question.Router.js";
-// import SolutionRouter from '../src/routers/Solution.Router.js';
-// import DiscussionRouter from '../src/routers/Discussion.Router.js'
-// // import { create } from "domain";
-
-
-// //API TESTING
-// app.get('/',(req, res)=>{
-//   res.json({message : "home"});
-// })
-
-// app.get('/test',(req, res)=>{
-//   res.json({message : "working"});
-// })
-
-// //use routers
-// app.use('/v1/public_api',AuthRouter);
-// app.use('/v1/private_api/auth',AuthRouter);
-// app.use('/v1/private_api/user',userRouter);
-// app.use('/v1/private_api/code',CodeRouter);
-// app.use('/v1/private_api/question',QuestionRouter);
-// app.use('/v1/private_api/solution',SolutionRouter);
-// app.use('/v1/private_api/discussion',DiscussionRouter);
-
-// //Global Error Middleware
-// app.use((err, req, res, next)=>{
-//         const status = err.status || 500;
-//         const message = err.message;
-//         const data = err.data || null;
-//         console.log("Error in GLOBAL ERROR MIDDLEWARE is ", err);
-//         return res.status(status).send({message, data});
-// })
-
-// //sockets
-// import { discussion_socket } from "../src/Sockets/Message.socket.js";
-// discussion_socket();
- 
-
-// export {server, io};
-
-
-
-
-// //Connect Database
-
-// connectDb()
-// .then(()=>{
-//     console.log("Connected to mongodb");
-//     setUpCloudinary();
-//     server.listen(process.env.PORT,()=>{
-//         console.log(`server is listening on ${process.env.PORT}`);
-//     });
-// })
-// .catch((error)=>{
-//     console.log("mongodb error",error)
-// })
 
 
 
--- src/app.js
+++ src/app.js
@@ -1,71 +1,71 @@
-// import cors from "cors";
-// import express from "express";
-// import { Server } from 'socket.io';
-// import {createServer} from 'http';
+import cors from "cors";
+import express from "express";
+import { Server } from 'socket.io';
+import {createServer} from 'http';
 
 
-// const app = express();
+const app = express();
 
-// const corsOptions ={
-//     origin: process.env.FRONT_END_URL ||  "http://localhost:3000", 
-//     methods: ["GET", "POST", "PUT", "DELETE"],
-//     credentials:true,            //access-control-allow-credentials:true
-//     optionSuccessStatus:200
-// };
-// app.use(cors(corsOptions));
-// app.use(express.json());
-// app.use(express.static('/public'));
-
-
-// const server = createServer(app);
-// const io = new Server(server, {
-//     cors: {
-//       origin: process.env.FRONT_END_URL ||  "http://localhost:3000",
-//       methods: ["GET", "POST", "PUT", "DELETE"],
-//       credentials: true,
-//     },
-//   });
-
-// //import routers
-// import userRouter from './routers/User.Router.js';
-// import AuthRouter from "./routers/Auth.Router.js";
-// import CodeRouter from "./routers/Code.Router.js";
-// import QuestionRouter from "./routers/Question.Router.js";
-// import SolutionRouter from './routers/Solution.Router.js';
-// import DiscussionRouter from './routers/Discussion.Router.js'
-// import { create } from "domain";
+const corsOptions ={
+    origin: process.env.FRONT_END_URL ||  "http://localhost:3000", 
+    methods: ["GET", "POST", "PUT", "DELETE"],
+    credentials:true,            //access-control-allow-credentials:true
+    optionSuccessStatus:200
+};
+app.use(cors(corsOptions));
+app.use(express.json());
+app.use(express.static('/public'));
 
 
-// //API TESTING
-// app.get('/',(req, res)=>{
-//   res.json({message : "home"});
-// })
+const server = createServer(app);
+const io = new Server(server, {
+    cors: {
+      origin: process.env.FRONT_END_URL ||  "http://localhost:3000",
+      methods: ["GET", "POST", "PUT", "DELETE"],
+      credentials: true,
+    },
+  });
 
-// app.get('/test',(req, res)=>{
-//   res.json({message : "working"});
-// })
+//import routers
+import userRouter from './routers/User.Router.js';
+import AuthRouter from "./routers/Auth.Router.js";
+import CodeRouter from "./routers/Code.Router.js";
+import QuestionRouter from "./routers/Question.Router.js";
+import SolutionRouter from './routers/Solution.Router.js';
+import DiscussionRouter from './routers/Discussion.Router.js'
+import { create } from "domain";
 
-// //use routers
-// app.use('/v1/public_api',AuthRouter);
-// app.use('/v1/private_api/auth',AuthRouter);
-// app.use('/v1/private_api/user',userRouter);
-// app.use('/v1/private_api/code',CodeRouter);
-// app.use('/v1/private_api/question',QuestionRouter);
-// app.use('/v1/private_api/solution',SolutionRouter);
-// app.use('/v1/private_api/discussion',DiscussionRouter);
 
-// //Global Error Middleware
-// app.use((err, req, res, next)=>{
-//         const status = err.status || 500;
-//         const message = err.message;
-//         const data = err.data || null;
-//         console.log("Error in GLOBAL ERROR MIDDLEWARE is ", err);
-//         return res.status(status).send({message, data});
-// })
+//API TESTING
+app.get('/',(req, res)=>{
+  res.json({message : "home"});
+})
 
-// //sockets
-// import { discussion_socket } from "./Sockets/Message.socket.js";
-// discussion_socket();
+app.get('/test',(req, res)=>{
+  res.json({message : "working"});
+})
+
+//use routers
+app.use('/v1/public_api',AuthRouter);
+app.use('/v1/private_api/auth',AuthRouter);
+app.use('/v1/private_api/user',userRouter);
+app.use('/v1/private_api/code',CodeRouter);
+app.use('/v1/private_api/question',QuestionRouter);
+app.use('/v1/private_api/solution',SolutionRouter);
+app.use('/v1/private_api/discussion',DiscussionRouter);
+
+//Global Error Middleware
+app.use((err, req, res, next)=>{
+        const status = err.status || 500;
+        const message = err.message;
+        const data = err.data || null;
+        console.log("Error in GLOBAL ERROR MIDDLEWARE is ", err);
+        return res.status(status).send({message, data});
+})
+
+//sockets
+import { discussion_socket } from "./Sockets/Message.socket.js";
+discussion_socket();
  
 
-// export {server, io};
+export {server, io};
