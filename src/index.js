b7e19c184c4f0de34de8cc2b637de547e692987f
swayam_op <swayamprakash97691@gmail.com>
2024-04-01 18:50:07 +0530

    vercel added again

--- src/index.js
+++ src/index.js
@@ -12,9 +12,9 @@ connectDb()
 .then(()=>{
     console.log("Connected to mongodb");
     setUpCloudinary();
-    // server.listen(process.env.PORT,()=>{
-    //     console.log(`server is listening on ${process.env.PORT}`);
-    // });
+    server.listen(process.env.PORT,()=>{
+        console.log(`server is listening on ${process.env.PORT}`);
+    });
 })
 .catch((error)=>{
     console.log("mongodb error",error)
